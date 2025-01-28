c34f67ceb39c68a6be209cc2cc4a83baa6cc1b77
RhexNiebres <rhexniebres@gmail.com>
2025-01-27 17:11:32 -0800

    Add PersonalDetailsForm component to PersonalDetailsSection component structure

---
+++ src/components/PersonalDetailsSection.jsx
@@ -0,0 +1,12 @@
+import PersonalDetailsForm from './PersonalDetailsForm'
+
+export default function PersonalDetailsSection(){
+    return(
+        <section>
+            <hr />
+            <h2>Personal Details</h2>
+            <hr />
+            <PersonalDetailsForm/>
+        </section>
+    )
+}
