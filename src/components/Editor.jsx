33c6a8bc7526d3886d35c5d81411413148a748ec
RhexNiebres <rhexniebres@gmail.com>
2025-01-27 17:10:43 -0800

    Add PersonalDetailSection to Editor component structure

---
+++ src/components/Editor.jsx
@@ -0,0 +1,13 @@
+import PersonalDetailsSection from './PersonalDetailsSection'
+
+export default function Editor(){
+    return(
+        <section>
+            <div />
+            <h1>Cv Builder</h1>
+            <div />
+            <PersonalDetailsSection/>
+            
+        </section>
+    )
+}
