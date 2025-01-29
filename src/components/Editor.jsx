a57be70dcdccf2c40748869353797fccacc55482
RhexNiebres <rhexniebres@gmail.com>
2025-01-29 15:36:30 -0800

    Pass handleChange function to components for handling input changes  and handleImageChange for form to handle image changes

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,13 +1,14 @@
 import EducationInput from './EducationInput';
-import Form from './PersonalDetailsForm';
+import Form from './PersonalInput.jsx';
 import PracticalFormInput from './PracticaInput.jsx';
 import '../styles/editor.css'
-export default function Editor({ formData, setFormData }) {
+
+export default function Editor({ formData, setFormData, handleChange, handleImageChange  }) {
   return (
     <section className='Editor'>
-      <Form data={formData} setFormData={setFormData} />
-      <EducationInput formData={formData} setFormData={setFormData} />
-      < PracticalFormInput formData={formData} setFormData={setFormData}/>
+      <Form data={formData} setFormData={setFormData} handleChange = {handleChange} handleImageChange = {handleImageChange}/>
+      <EducationInput formData={formData} setFormData={setFormData} handleChange = {handleChange}/>
+      < PracticalFormInput formData={formData} setFormData={setFormData} handleChange = {handleChange}/>
     </section>
   );
 }
