23953f9514b3006627fd0611b5dca150a4e668a8
RhexNiebres <rhexniebres@gmail.com>
2025-01-28 19:17:46 -0800

    Add PersonalDetailsSection component to display personal details and profile picture with print functionality

--- src/components/PersonalDetailsSection.jsx
+++ src/components/PersonalDetailsSection.jsx
@@ -1,12 +1,41 @@
-import PersonalDetailsForm from './PersonalDetailsForm'
 
-export default function PersonalDetailsSection(){
-    return(
-        <section>
-            <hr />
-            <h2>Personal Details</h2>
-            <hr />
-            <PersonalDetailsForm/>
-        </section>
-    )
+export default function PersonalDetailsSection({ formData }) {
+  const { firstName, lastName, email, phoneNumber, address, jobRole, about, profilePicture,} = formData;
+  const profilePictureToDisplay = profilePicture || './assets/sponge.jpg';
+
+  const handlePrint = () => {
+    window.print();
+  };
+
+  return (
+    <div className='PersonalOutput'>
+      <h3>Preview</h3>
+      {profilePicture && (
+        <div>
+          <img src={profilePictureToDisplay} alt="Profile" style={{ width: '150px', height: 'auto' }} />
+        </div>
+      )}
+      <p>
+        Full Name: <b>{firstName} {lastName}</b>
+      </p>
+      <p>
+        About: <b>{about}</b>
+      </p>
+      <p>
+        Email: <b>{email}</b>
+      </p>
+      <p>
+        Phone Number: <b>{phoneNumber}</b>
+      </p>
+      <p>
+        Address: <b>{address}</b>
+      </p>
+      <p>
+        Job Role: <b>{jobRole}</b>
+      </p>
+      <button className="print" onClick={handlePrint}>Print</button>
+    </div>
+  );
 }
+
+
