72fe48e6716593b47f40113422f7643ef7212428
RhexNiebres <rhexniebres@gmail.com>
2025-01-28 19:19:11 -0800

    Create Preview component to display sections for personal details, education, and practical experience

---
+++ src/components/Preview.jsx
@@ -0,0 +1,19 @@
+import '../styles/Preview.css';
+import EducationFormOutput from './EducationOutput.jsx'
+import  PersonalDetailsSection from './PersonalDetailsSection.jsx'
+import PracticalFormOutput from './PracticalOutput.jsx';
+
+
+export default function Preview ({ formData }){
+    return(
+        <section className='Preview'>
+            <PersonalDetailsSection formData={formData}/>
+            <hr />
+            <EducationFormOutput formData={formData}/>
+            <hr />
+            <PracticalFormOutput formData={formData}/>
+            <hr />
+
+        </section>
+    )
+}
